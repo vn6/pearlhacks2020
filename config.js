ad4cc52d0d92c981c2a27ed217665cc2a616efd2
vn6 <34071587+vn6@users.noreply.github.com>
2020-02-23 08:39:27 -0500

    imported from gcloud

---
+++ config.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+          apiKey: "AIzaSyCqEbjgGMv8PqumM8IGqWKSHWbKgfT_lQo",
+          authDomain: "pearlhacks2020-269022.firebaseapp.com",
+          databaseURL: "https://pearlhacks2020-269022.firebaseio.com",
+          projectId: "pearlhacks2020-269022",
+          storageBucket: "pearlhacks2020-269022.appspot.com",
+          messagingSenderId: "928809181787",
+          appId: "1:928809181787:web:fc090715993f5f0fb8157e",
+          measurementId: "G-3Z74V7EMEX"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// -------------- serve static folders -------------- //
+app.use('/views', express.static(path.join(__dirname, 'views')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/img', express.static(path.join(__dirname, 'img')));
+
+// ------------- express get handlers ---------------- //
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
+app.get('/:page', (req, res) => {
+  res.redirect('http://pearlhacks2020-269022.appspot.com/');
+});
+
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
+
